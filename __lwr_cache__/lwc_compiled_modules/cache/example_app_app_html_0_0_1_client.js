9895976b21c605e04a9d648d541e90afff6d651a
Bobbygllh <bobbygllh@gmail.com>
2025-10-07 08:32:49 -0400

    committed

--- __lwr_cache__/lwc_compiled_modules/cache/example_app_app_html_0_0_1_client.js
+++ __lwr_cache__/lwc_compiled_modules/cache/example_app_app_html_0_0_1_client.js
@@ -1,7 +1,7 @@
 import _implicitStylesheets from "./app.css";
 import _implicitScopedStylesheets from "./app.scoped.css?scoped=true";
 import {freezeTemplate, parseFragment, registerTemplate} from "lwc";
-const $fragment1 = parseFragment`<main${3}><img src="/public/assets/recipes-logo.png" alt="logo"${3}><h1${3}>Hello World!</h1></main>`;
+const $fragment1 = parseFragment`<main${3}><img src="/public/assets/recipes-logo.png" alt="logo"${3}><h1${3}>Hello World!@</h1></main>`;
 function tmpl($api, $cmp, $slotset, $ctx) {
   const {st: api_static_fragment} = $api;
   return [api_static_fragment($fragment1, 1)];
@@ -9,8 +9,8 @@ function tmpl($api, $cmp, $slotset, $ctx) {
 }
 export default registerTemplate(tmpl);
 tmpl.stylesheets = [];
-tmpl.stylesheetToken = "lwc-5la2ic78dit";
-tmpl.legacyStylesheetToken = "example-app_app";
+tmpl.stylesheetToken = "lwc-4us9q5akfb0";
+tmpl.legacyStylesheetToken = "-app_app";
 if (_implicitStylesheets) {
   tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitStylesheets);
 }
