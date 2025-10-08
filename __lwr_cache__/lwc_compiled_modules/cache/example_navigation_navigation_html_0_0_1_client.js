7b046bc5e5ddecfd664022ed1fd0a609dd1e2f45
Bobbygllh <bobbygllh@gmail.com>
2025-10-08 17:31:22 -0400

    Updated the file path from /public to public

--- __lwr_cache__/lwc_compiled_modules/cache/example_navigation_navigation_html_0_0_1_client.js
+++ __lwr_cache__/lwc_compiled_modules/cache/example_navigation_navigation_html_0_0_1_client.js
@@ -1,7 +1,7 @@
 import _implicitStylesheets from "./navigation.css";
 import _implicitScopedStylesheets from "./navigation.scoped.css?scoped=true";
 import {freezeTemplate, parseFragment, registerTemplate} from "lwc";
-const $fragment1 = parseFragment`<header${3}><nav class="nav${0}"${2}><div class="left${0}"${2}><img class="initials-image${0}" src="/public/assets/bg.svg" alt="logo"${2}></div><div class="right${0}"${2}><a href="#" title="Home" data-item="mainpage"${3}><span${3}>Home</span></a><a href="#" title="Projects" data-item="projects"${3}><span${3}>Projects</span></a><a href="#" title="Blog" data-item="blogs"${3}><span${3}>Blog</span></a><a href="https://github.com/bobbygllh" target="_blank" title="Github" class="lt-md:hidden${0}"${2}><img class="github-image${0}" src="/public/assets/github-mark.png" alt="logo"${2}></a></div></nav></header>`;
+const $fragment1 = parseFragment`<header${3}><nav class="nav${0}"${2}><div class="left${0}"${2}><img class="initials-image${0}" src="public/assets/bg.svg" alt="logo"${2}></div><div class="right${0}"${2}><a href="#" title="Home" data-item="mainpage"${3}><span${3}>Home</span></a><a href="#" title="Projects" data-item="projects"${3}><span${3}>Projects</span></a><a href="#" title="Blog" data-item="blogs"${3}><span${3}>Blog</span></a><a href="https://github.com/bobbygllh" target="_blank" title="Github" class="lt-md:hidden${0}"${2}><img class="github-image${0}" src="public/assets/github-mark.png" alt="logo"${2}></a></div></nav></header>`;
 function tmpl($api, $cmp, $slotset, $ctx) {
   const {b: api_bind, sp: api_static_part, st: api_static_fragment} = $api;
   const {_m0, _m1, _m2} = $ctx;
@@ -22,8 +22,8 @@ function tmpl($api, $cmp, $slotset, $ctx) {
 }
 export default registerTemplate(tmpl);
 tmpl.stylesheets = [];
-tmpl.stylesheetToken = "lwc-5390kdmit7f";
-tmpl.legacyStylesheetToken = "-navigation_navigation";
+tmpl.stylesheetToken = "lwc-5l9k039pvit";
+tmpl.legacyStylesheetToken = "example-navigation_navigation";
 if (_implicitStylesheets) {
   tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitStylesheets);
 }
--- src/modules/example/projects/items.js
+++ src/modules/example/projects/items.js
@@ -2,13 +2,13 @@ const allProjectItems = [
     {
         title: 'Inbound API Integration',
         description: "This project is the creation of a end to end custom REST inbound api that inserts a Case",
-        image: "/public/assets/api-integration.webp",
+        image: "public/assets/api-integration.webp",
         lwcReference: "caseApi"
     },
      {
         title: 'Building a LWC OSS Website',
         description: "Walkthrough of building this personal website with Lightning Web Component OSS and Github Pages",
-        image: "/public/assets/lwc.png",
+        image: "public/assets/lwc.png",
         lwcReference: "lwcOSS"
     }
 ]
